aa6096f4db7f770aeffebe8d0e5158607db10c38
Ing. Jiří Daněček <jiri.danecek@fit.cvut.cz>
2020-11-19 14:20:10 +0100

    deltarget

--- elems/src/main/kotlin/elems/ElemsData.kt
+++ elems/src/main/kotlin/elems/ElemsData.kt
@@ -1,50 +1,74 @@
 package elems
 
-data class Elem2 {
-    override fun toString(): String = content.joinToString(separator = "\n")
+import java.util.*
 
-    val content: Array<String>
+abstract class Elem2 {
 
-    val w : Int
+    protected fun myToString(): String = content.joinToString(separator = "\n")
+
+    open abstract val content: Array<String>
+
+    open val w: Int
         get() = content[0].length
-    val h : Int
+    open val h: Int
         get() = content.size
 
-    constructor(ch: String, w: Int, h: Int) {
-        content = Array(h) { (ch.repeat(w)) }
-    }
-
-    constructor(w: Int, h: Int) : this(" ", w, h)
-
-    constructor(_content: Array<String>) {
-        content = _content
-    }
-
-    infix fun above(that: Elem): Elem {
+    infix fun above(that: Elem2): Elem2 {
         assert(that.w == w)
-        return Elem(this.content + that.content)
-        //Array(this.h() + that.h()) {   }
+        return BasicElem(this.content + that.content)
     }
 
-    infix fun beside(that: Elem): Elem {
+    infix fun beside(that: Elem2): Elem2 {
         assert(that.w == w)
-        return Elem(Array(h){ i -> this.content[i] + that.content[i]  })
+        return BasicElem(Array(h) { i -> this.content[i] + that.content[i] })
     }
 
-    infix fun widenR(newW: Int): Elem {
+
+    infix fun widenR(newW: Int): Elem2 {
         assert(w <= newW)
-        return this beside Elem(newW- w, h)
+        return this beside empty(newW - w, h)
 
     }
-    infix fun widenL(newW: Int): Elem {
+
+    infix fun widenL(newW: Int): Elem2 {
         assert(w <= newW)
-        return Elem(newW- w, h)  beside this
+        return empty(newW - w, h) beside this
+    }
+
+    companion object {
+        fun char(ch: Char, w: Int, h: Int) = BasicElem(Array(h) { ch.toString().repeat(w) })
+        fun empty(w: Int, h: Int) = char(' ', w, h)
+        val magicSquare = BasicElem(arrayOf("123", "456", "789"))
+    }
+}
+
+data class BasicElem(override val content: Array<String>) : Elem2() {
+    override fun toString(): String = myToString()
+
+    override fun equals(other: Any?): Boolean {
+        return if (other !is BasicElem) false
+        else Arrays.equals(content, other.content)
+    }
+
+    object MagicSquare2 : Elem2() {
+        override val content: Array<String> = arrayOf("123", "456", "789")
+    }
+}
+
+object MagicSquare3 : Elem2() {
+    override val content: Array<String> = arrayOf("123", "456", "789")
+}
+
+fun triangle(n: Int): Elem2 {
+    return if (n == 1) Elem2.char('*', 1, 1) else {
+        val subelem = triangle(n - 1)
+        subelem above Elem2.char('*', n, 1)
     }
 }
 
 fun main() {
-    val e1 = Elem(arrayOf("abc", "xyz"))
-    val e2 = Elem("*", 6, 4)
-    println( e1.widenL(e2.w) above e2)
+    val t4 = triangle(4)
+    println(t4 == triangle(4))
+    println(t4)
 
 }
