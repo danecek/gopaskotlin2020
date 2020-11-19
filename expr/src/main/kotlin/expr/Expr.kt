27c6f87a0341c65f676d0cf5bc8626e6adc0e0e4
Ing. Jiří Daněček <jiri.danecek@fit.cvut.cz>
2020-11-19 17:16:00 +0100

    deltarget

--- expr/src/main/kotlin/expr/Expr.kt
+++ expr/src/main/kotlin/expr/Expr.kt
@@ -4,28 +4,35 @@ import expr.Oper.*
 import java.lang.UnsupportedOperationException
 
 interface ExprPatt {
-    fun match(e: Expr): Expr?
+    fun match(e: Expr?): Expr?
 }
 
 open class UnOpPat(val op: Oper) : ExprPatt {
-    override fun match(e: Expr): UnOp? {
+    override fun match(e: Expr?): UnOp? {
         return if (e is UnOp && e.op == op) e else null
     }
 }
+
+
 
 object UnPlusPat : UnOpPat(PLUS)
 
 object UnMnsMns : UnOpPat(MINUS)
 
-object UnMnsMnsPat : ExprPatt {
-    override fun match(e: Expr): UnOp? {
-        return if (e !is UnOp || e.op != MINUS) null
-        else {
-            val eoprnd = e.oprnd;
-            if (eoprnd !is UnOpPat || eoprnd.op != MINUS) null
-            else e
-        }
+class NumPatt(val value: Int) : ExprPatt {
+    override fun match(e: Expr?): Expr? = if ((e as? Num)?.value == value) e else null
+}
+
+object PlusZeroPat : ExprPatt {
+    override fun match(e: Expr?): Expr? {
+        return if ((e as? BinOp)?.op == PLUS)
     }
+
+
+}
+
+object UnMnsMnsPat : ExprPatt {
+    override fun match(e: Expr?): UnOp? = UnMnsMnsPat.match(UnMnsMnsPat.match(e))
 }
 
 interface Expr {
@@ -34,6 +41,16 @@ interface Expr {
 
 data class Num(override val value: Int) : Expr {
     override fun toString() = value.toString()
+}
+
+data class BinOp(val op: Oper, val left: Expr, val right: Expr) : Expr {
+    override val value = when (op) {
+        MINUS -> left.value - right.value
+        PLUS -> left.value + right.value
+        else -> throw UnsupportedOperationException()
+    }
+
+    override fun toString() = "$op$oprnd"
 }
 
 data class UnOp(val op: Oper, val oprnd: Expr) : Expr {
@@ -47,9 +64,7 @@ data class UnOp(val op: Oper, val oprnd: Expr) : Expr {
 }
 
 fun Expr.optim(): Expr {
-    return UnPlusPat.match(this)?.oprnd?.optim() ?:
-    ((UnMnsMnsPat.match(this)?.oprnd as? UnOp))?.oprnd?.optim() ?:
-    this
+    return UnPlusPat.match(this)?.oprnd?.optim() ?: ((UnMnsMnsPat.match(this)?.oprnd as? UnOp))?.oprnd?.optim() ?: this
 }
 
 fun main() {
