517896ca886b3d01ed3e9b3a4557e7af8b4035d1
Ing. Jiří Daněček <jiri.danecek@fit.cvut.cz>
2020-11-19 15:08:19 +0100

    expr

--- expr/src/main/kotlin/expr/Expr.kt
+++ expr/src/main/kotlin/expr/Expr.kt
@@ -1,15 +1,28 @@
 package expr
 
+import expr.Oper.*
+
 interface Expr {
-   val value: Int
+    val value: Int
 }
 
-data class Num(override val value: Int) : Expr
+data class Num(override val value: Int) : Expr {
+    override fun toString()= value.toString()
+}
 
+data class UnOp(val op: Oper, val oprnd: Expr) : Expr {
+    override val value = when (op) {
+        MINUS -> -oprnd.value
+        PLUS -> oprnd.value
+    }
+    override fun toString() = "$op$oprnd"
+}
 
 fun main() {
-    println(Num(3).value)
-    val e1 = UnOp(Oper.MINUS, Num(3))
-    println(e1.value) // -3
-    println(e1.toString)  // -3
+    val unmns = UnOp(MINUS, Num(3))
+    println(unmns.value) // -3
+    println(unmns.toString())  // -3
+    val unpls = unmns.copy(op = PLUS)
+    println(unpls)
+    println(unmns == UnOp(MINUS, Num(3)))
 }
--- expr/src/main/kotlin/expr/Oper.kt
+++ expr/src/main/kotlin/expr/Oper.kt
@@ -1,5 +1,18 @@
 package expr
 
 enum class Oper {
+    MINUS {
+        override fun toString(): String = "-"
+    },
+    PLUS {
+        override fun toString(): String = "+"
+    }
+}
+
+enum class OperVariant(val repr: String) {
+    PLUS("-"),
+    MINUS("-");
+
+    override fun toString(): String = repr
 
 }
