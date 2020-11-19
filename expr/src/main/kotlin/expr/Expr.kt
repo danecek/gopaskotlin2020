b79dd9bc04f8ae4142063ca81bfd3f3503dd46f6
Ing. Jiří Daněček <jiri.danecek@fit.cvut.cz>
2020-11-19 16:40:02 +0100

    deltarget

--- expr/src/main/kotlin/expr/Expr.kt
+++ expr/src/main/kotlin/expr/Expr.kt
@@ -4,26 +4,27 @@ import expr.Oper.*
 import java.lang.UnsupportedOperationException
 
 interface ExprPatt {
-   fun match(e: Expr) : Expr?
+    fun match(e: Expr): Expr?
 }
 
-object UnivPatt : ExprPatt {
-    override fun match(e: Expr): Expr? = e
-}
-object UnPat: ExprPatt{
-    override fun match(e: Expr): UnOp? = e as? UnOp
-}
-
-object UnPlusPat: ExprPatt{
+open class UnOpPat(val op: Oper) : ExprPatt {
     override fun match(e: Expr): UnOp? {
-       return if (e is UnOp && e.op == PLUS) e else null
+        return if (e is UnOp && e.op == op) e else null
     }
 }
 
-class UnOpPat(val op: Oper): ExprPatt{
+object UnPlusPat : UnOpPat(PLUS)
 
+object UnMnsMns : UnOpPat(MINUS)
+
+object UnMnsMnsPat : ExprPatt {
     override fun match(e: Expr): UnOp? {
-        return if (e is UnOp && e.op == op) e else null
+        return if (e !is UnOp || e.op != MINUS) null
+        else {
+            val eoprnd = e.oprnd;
+            if (eoprnd !is UnOpPat || eoprnd.op != MINUS) null
+            else e
+        }
     }
 }
 
@@ -32,7 +33,7 @@ interface Expr {
 }
 
 data class Num(override val value: Int) : Expr {
-    override fun toString()= value.toString()
+    override fun toString() = value.toString()
 }
 
 data class UnOp(val op: Oper, val oprnd: Expr) : Expr {
@@ -41,18 +42,22 @@ data class UnOp(val op: Oper, val oprnd: Expr) : Expr {
         PLUS -> oprnd.value
         else -> throw UnsupportedOperationException()
     }
+
     override fun toString() = "$op$oprnd"
 }
 
-fun Expr.optim() : Expr {
-    return UnOpPat(PLUS).match(this)?.oprnd?.optim() ?:
+fun Expr.optim(): Expr {
+    return UnPlusPat.match(this)?.oprnd?.optim() ?:
+    ((UnMnsMnsPat.match(this)?.oprnd as? UnOp))?.oprnd?.optim() ?:
     this
 }
 
 fun main() {
-    val unmns = UnOp(MINUS, Num(3))
+    val unmns = UnOp(MINUS, UnOp(MINUS, Num(3)))
     println(unmns.value) // -3
     println(unmns.toString())  // -3
+    println(unmns.optim())  // -3
+    println(unmns.optim().value)  // -3
     val unpls = unmns.copy(op = PLUS)
     println(unpls)
     println(unpls.optim())
