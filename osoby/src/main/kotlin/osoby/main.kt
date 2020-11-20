01c79ce2fbabfcd7dfb0f6534dff85721a3ece27
Ing. Jiří Daněček <jiri.danecek@fit.cvut.cz>
2020-11-20 11:12:12 +0100

    osoba

--- osoby/src/main/kotlin/osoby/main.kt
+++ osoby/src/main/kotlin/osoby/main.kt
@@ -4,21 +4,24 @@ import java.util.*
 
 class Osoba(val name: String, var rodic: Osoba? = null, val deti : MutableMap<String, Osoba> = mutableMapOf()) {
 
-
    fun addPotomek(potomek: Osoba) {
        potomek.rodic = this
        deti[potomek.name] = potomek
    }
 
-   fun najdiVnukaC(diteS: String, vnukS: String) : Osoba? {
+   fun najdiVnukaClassik(diteS: String, vnukS: String) : Osoba? {
 
    }
-    fun najdiVnukaO(diteS: String, vnukS: String) : Optional<Osoba> {
+    fun najdiVnukaFunctional(diteS: String, vnukS: String) : Optional<Osoba> {
 
     }
-    fun najdiVnukaK(diteS: String, vnukS: String) : Osoba? {
+    fun najdiVnukaKotlin(diteS: String, vnukS: String) : Osoba? {
 
     }
+}
+
+fun Osoba.rodic(name: String, potomci: Osoba.(name: String) -> Unit) {
+
 }
 
 fun main(args: Array<String>) {
@@ -28,4 +31,19 @@ fun main(args: Array<String>) {
     val vnuk = Osoba("vnuk")
     ded.addPotomek(otec)
     otec.addPotomek(vnuk)
+    val patr = patriarcha("praded") {
+        this.rodic("ded1") {
+            this.rodic("otec") {
+                this. potomek("vnuk1")
+                this.potomek("vnuk2")
+            }
+            this.rodic("matka") {
+
+            }
+        }
+        this.rodic("ded2") {
+            this.potomek("otec2")
+        }
+    }
+    println(ded.najdiVnukaClassik("otec", "vnuk"))
 }
